5f97bdbbadd3f6056d84a57f6b920daa48aff60c
simon <simon.gobin.do@gmail.com>
2025-06-12 13:18:41 +0100

    integrat telgram with polling

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,28 @@
 # app/bot.py
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from telegram import Update
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-app = ApplicationBuilder().token(TOKEN).build()
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello from the bot!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-app.add_handler(CommandHandler("start", start))
 
-def run_telegram_bot():
-    app.run_polling()
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
